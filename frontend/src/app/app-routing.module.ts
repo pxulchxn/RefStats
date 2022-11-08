fa3b87bf19ade7f4d616fc6d3ab91b84bfc104c4
Paul Czerwenka <p.czerwenka@ostfalia.de>
2022-11-08 11:31:20 +0100

    auth centred

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,22 +1,34 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { TestComponent } from "./test/test.component";
-import { AppLayoutComponent } from "./layout/app.layout.component";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {TestComponent} from "./test/test.component";
+import {AppLayoutComponent} from "./layout/app.layout.component";
 
 const routes: Routes = [
   {
     path: '', component: AppLayoutComponent,
     children: [
       {path: '', component: TestComponent},
-      {path: '', loadChildren: () => import('./program/components/dashboards/dashboards.module').then(m => m.DashboardsModule) },
-      {path: '', loadChildren: () => import('./program/components/auth/auth.module').then(m => m.AuthModule) },
-      {path: '', loadChildren: () => import('./program/components/settings/settings.module').then(m => m.SettingsModule) }
+      {
+        path: '',
+        loadChildren: () => import('./program/components/dashboards/dashboards.module').then(m => m.DashboardsModule)
+      },
+      {
+        path: '',
+        loadChildren: () => import('./program/components/settings/settings.module').then(m => m.SettingsModule)
+      }
     ]
-  },{path: 'landing', loadChildren: () => import('./program/components/landing/landing.module').then(m => m.LandingModule) },
+  }, {
+    path: 'landing',
+    loadChildren: () => import('./program/components/landing/landing.module').then(m => m.LandingModule)
+  }, {
+    path: 'auth',
+    loadChildren: () => import('./program/components/auth/auth.module').then(m => m.AuthModule)
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
